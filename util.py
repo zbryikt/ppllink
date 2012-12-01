fdd1425891d12cec978c0eda58a8d602c11b6955
Pei-Lun Lee <ypcat6@gmail.com>
2012-12-01 14:42:27 +0800

    add import export command

---
+++ util.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import json
+import pymongo
+import sys
+
+def main():
+    if len(sys.argv) < 2 or sys.argv[1] not in ['import', 'export']:
+        print 'usage: %s import|export [db.json]' % sys.argv[0]
+        return 0
+
+    if len(sys.argv) < 3:
+        filename = 'db.json'
+    else:
+        filename = sys.argv[2]
+
+    db = pymongo.Connection().ppllink
+
+    if sys.argv[1] == 'import':
+        data = json.load(open(filename))
+        for node in data.get('node'):
+            db.node.save(node)
+        for link in data.get('link'):
+            db.link.save(link)
+    elif sys.argv[1] == 'export':
+        nodes = list(db.node.find({}, {'_id':0}))
+        links = list(db.link.find({}, {'_id':0}))
+        json.dump({'node':nodes, 'link':links}, open(filename, 'w'), indent=2)
+    else:
+        assert 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
